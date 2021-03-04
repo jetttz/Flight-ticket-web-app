89bc861e59293bbc91ec5e84c776f979d7c96cde
Jet <61175627+jetttz@users.noreply.github.com>
2021-03-03 21:46:34 -0600

    Update db.js

--- Flight_ticket_web_app/server/db.js
+++ Flight_ticket_web_app/server/db.js
@@ -9,11 +9,11 @@ var getstuff = fs.readFileSync('password.txt','utf8');
 var getname = getstuff.split('\n');
 
 const pool = new Pool({
-  host: 'code.cs.uh.edu',
+  host: 'hostname',
   user: getname[0],
   password: getname[1],
   port: 5432,
-  database: 'COSC3380'
+  database: 'databasename'
 });
 
 module.exports = pool;
